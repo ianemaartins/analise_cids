ae930beefd54a2b5f37c3b28b08334c0f41ee1ef
Iane Martins <ianemartins99@gmail.com>
2024-09-18 17:54:33 -0300

    adicionando graficos da analise bivariada linhaii e causabas br

--- bivariada_linhaii_causabas.R
+++ bivariada_linhaii_causabas.R
@@ -26,7 +26,7 @@ categorizar_cid <- function(causa) {
   } else if (causa_principal >= "G00" & causa_principal <= "G99") {
     return("Doenças do sistema nervoso  (G00-G99)")
   } else if (causa_principal >= "H00" & causa_principal <= "H59") {
-    return("Doenças dos olhos  (H00-H59")
+    return("Doenças dos olhos  (H00-H59)")
   } else if (causa_principal >= "H60" & causa_principal <= "H95") {
     return("Doenças do ouvido e apófise mastoide  (H60-H95")
   } else if (causa_principal >= "I00" & causa_principal <= "I99") {
@@ -34,7 +34,7 @@ categorizar_cid <- function(causa) {
   } else if (causa_principal >= "J00" & causa_principal <= "J99") {
     return("Doenças do aparelho respiratório  (J00-J99)")
   } else if (causa_principal >= "K00" & causa_principal <= "K93") {
-    return("Doenças do aparelho digestivo  (K00-K93")
+    return("Doenças do aparelho digestivo  (K00-K93)")
   } else if (causa_principal >= "L00" & causa_principal <= "L99") {
     return("Doenças da pele e tecido subcutâneo  (L00-L99)")
   } else if (causa_principal >= "M00" & causa_principal <= "M99") {
@@ -98,4 +98,299 @@ pasta <- "graficos_comparacao/gbarra_linhaii_causabas.html"
 saveWidget(gbarra_linhaii_causabas, pasta)
 
 
+#-------------- Analise mais aprofundada CID I00-I99 -----------------
+
+df_i00ai99 <- dados_br_linhaii %>%
+  filter(categoria_causabas == "Doenças do aparelho circulatório  (I00-I99)")
+
+# Função para categorizar os códigos CID I00-I99 , considerando apenas os três primeiros caracteres
+categorizar_cidi00ai99 <- function(causa) {
+  # Extrair os três primeiros caracteres
+  causa_principal <- substr(causa, 1, 3)
+  
+  if (causa_principal >= "I00" & causa_principal <= "I02") {
+    return("Febre reumática aguda")
+  } else if (causa_principal >= "I05" & causa_principal <= "I09") {
+    return("Doenças reumáticas crônicas do coração")
+  } else if (causa_principal >= "I10" & causa_principal <= "I15") {
+    return("Doenças hipertensivas")
+  } else if (causa_principal >= "I20" & causa_principal <= "I25") {
+    return("Doenças isquêmicas do coração")
+  } else if (causa_principal >= "I26" & causa_principal <= "I28") {
+    return("Doenças cardíaca pulmonar e da circulação pulmonar")
+  } else if (causa_principal >= "I30" & causa_principal <= "I52") {
+    return("Outras formas de doença do coração")
+  } else if (causa_principal >= "I60" & causa_principal <= "I69") {
+    return("Doenças cerebrovasculares")
+  } else if (causa_principal >= "I70" & causa_principal <= "I79") {
+    return("Doenças das artérias, das arteríolas e dos capilares")
+  } else if (causa_principal >= "I80" & causa_principal <= "I89") {
+    return("Doenças das veias, dos vasos linfáticos e dos gânglios linfáticos, não classificadas em outra parte")
+  } else if (causa_principal >= "I95" & causa_principal <= "I99") {
+    return("Outros transtornos, e os não especificados do aparelho circulatório")
+  } else {
+    return("Categoria desconhecida")
+  }
+}
+
+# Aplicar a função de categorização
+df_i00ai99$categoria_i00ai99 <- sapply(df_i00ai99$CAUSABAS, categorizar_cidi00ai99)
+
+
+#Agrupar dados da coluna categoria_causabas
+group_i00ai99 <- df_i00ai99 %>%
+  group_by(categoria_i00ai99) %>%
+  summarize(quantidade = n()) %>%
+  arrange(desc(quantidade))
+
+# Reordenar a variável 
+group_i00ai99$categoria_i00ai99 <- factor(
+  group_i00ai99$categoria_i00ai99, 
+  levels = group_i00ai99$categoria_i00ai99[order(group_i00ai99$quantidade, decreasing = FALSE)]
+)
+
+
+#plotar grafico plotly com as cids no eixo y e a quantidade do eixo x
+gbarra_i00ai99 <- plot_ly(
+  data = group_i00ai99,
+  x = ~quantidade,
+  y = ~categoria_i00ai99,
+  type = 'bar',
+  orientation = 'h'  # 'h' para horizontal
+)
+
+# Ajustar layout
+gbarra_i00ai99 <-gbarra_i00ai99 %>%
+  layout(
+    xaxis = list(title = 'Número de Óbitos'),
+    yaxis = list(title = 'Causa Básica do Óbito')
+  )
+
+# Salvar o gráfico HTML
+pasta <- "graficos_comparacao/gbarra_i00ai99.html"
+saveWidget(gbarra_i00ai99, pasta)
+
+
+
+#-------------- Analise mais aprofundada CID J00-J99 -----------------
+
+df_j00aj99 <- dados_br_linhaii %>%
+  filter(categoria_causabas == "Doenças do aparelho respiratório  (J00-J99)")
+
+# Função para categorizar os códigos CID J00-J99 , considerando apenas os três primeiros caracteres
+categorizar_cidj00aj99 <- function(causa) {
+  # Extrair os três primeiros caracteres
+  causa_principal <- substr(causa, 1, 3)
+  
+  if (causa_principal >= "J00" & causa_principal <= "J06") {
+    return("Infecções agudas das vias aéreas superiores")
+  } else if (causa_principal >= "J09" & causa_principal <= "J18") {
+    return("Influenza [gripe] e pneumonia")
+  } else if (causa_principal >= "J20" & causa_principal <= "J22") {
+    return("Outras infecções agudas das vias aéreas inferiores")
+  } else if (causa_principal >= "J30" & causa_principal <= "J39") {
+    return("Outras doenças das vias aéreas superiores")
+  } else if (causa_principal >= "J40" & causa_principal <= "J47") {
+    return("Doenças crônicas das vias aéreas inferiores")
+  } else if (causa_principal >= "J60" & causa_principal <= "J70") {
+    return("Doenças pulmonares devidas a agentes externos")
+  } else if (causa_principal >= "J80" & causa_principal <= "J84") {
+    return("Outras doenças respiratórias que afetam principalmente o interstício")
+  } else if (causa_principal >= "J85" & causa_principal <= "J86") {
+    return("Afecções necróticas e supurativas das vias aéreas inferiores")
+  } else if (causa_principal >= "J90" & causa_principal <= "J94") {
+    return("Outras doenças da pleura")
+  } else if (causa_principal >= "J95" & causa_principal <= "J99") {
+    return("Outras doenças do aparelho respiratório")
+  } else {
+    return("Categoria desconhecida")
+  }
+}
+
+# Aplicar a função de categorização
+df_j00aj99$categoria_j00aj99 <- sapply(df_j00aj99$CAUSABAS, categorizar_cidj00aj99)
+
+
+#Agrupar dados da coluna categoria_causabas
+group_j00aj99 <- df_j00aj99 %>%
+  group_by(categoria_j00aj99) %>%
+  summarize(quantidade = n()) %>%
+  arrange(desc(quantidade))
+
+# Reordenar a variável 
+group_j00aj99$categoria_j00aj99 <- factor(
+  group_j00aj99$categoria_j00aj99, 
+  levels = group_j00aj99$categoria_j00aj99[order(group_j00aj99$quantidade, decreasing = FALSE)]
+)
+
+
+#plotar grafico plotly com as cids no eixo y e a quantidade do eixo x
+gbarra_j00aj99 <- plot_ly(
+  data = group_j00aj99,
+  x = ~quantidade,
+  y = ~categoria_j00aj99,
+  type = 'bar',
+  orientation = 'h'  # 'h' para horizontal
+)
+
+# Ajustar layout
+gbarra_j00aj99 <-gbarra_j00aj99 %>%
+  layout(
+    xaxis = list(title = 'Número de Óbitos'),
+    yaxis = list(title = 'Causa Básica do Óbito')
+  )
+
+# Salvar o gráfico HTML
+pasta <- "graficos_comparacao/gbarra_j00aj99.html"
+saveWidget(gbarra_j00aj99, pasta)
+
+
+
+#-------------- Analise mais aprofundada CID K00-K93 -----------------
+
+df_k00ak93 <- dados_br_linhaii %>%
+  filter(categoria_causabas == "Doenças do aparelho digestivo  (K00-K93)")
+
+# Função para categorizar os códigos CID J00-J99 , considerando apenas os três primeiros caracteres
+categorizar_cidk00ak93 <- function(causa) {
+  # Extrair os três primeiros caracteres
+  causa_principal <- substr(causa, 1, 3)
+  
+  if (causa_principal >= "K00" & causa_principal <= "K14") {
+    return("Doenças da cavidade oral, das glândulas salivares e dos maxilares")
+  } else if (causa_principal >= "K20" & causa_principal <= "K31") {
+    return("Doenças do esôfago, do estômago e do duodeno")
+  } else if (causa_principal >= "K35" & causa_principal <= "K38") {
+    return("Doenças do apêndice")
+  } else if (causa_principal >= "K40" & causa_principal <= "K46") {
+    return("Hérnias")
+  } else if (causa_principal >= "K50" & causa_principal <= "K52") {
+    return("Enterites e colites não-infecciosas")
+  } else if (causa_principal >= "K55" & causa_principal <= "K63") {
+    return("Outras doenças dos intestinos")
+  } else if (causa_principal >= "K65" & causa_principal <= "K67") {
+    return("Doenças do peritônio")
+  } else if (causa_principal >= "K70" & causa_principal <= "K77") {
+    return("Doenças do fígado")
+  } else if (causa_principal >= "K80" & causa_principal <= "K87") {
+    return("Transtornos da vesícula biliar, das vias biliares e do pâncreas")
+  } else if (causa_principal >= "K90" & causa_principal <= "K93") {
+    return("Outras doenças do aparelho digestivo")
+  } else {
+    return("Categoria desconhecida")
+  }
+}
+
+# Aplicar a função de categorização
+df_k00ak93$categoria_k00ak93 <- sapply(df_k00ak93$CAUSABAS, categorizar_cidk00ak93)
+
+
+#Agrupar dados da coluna categoria_causabas
+group_k00ak93 <- df_k00ak93 %>%
+  group_by(categoria_k00ak93) %>%
+  summarize(quantidade = n()) %>%
+  arrange(desc(quantidade))
+
+# Reordenar a variável 
+group_k00ak93$categoria_k00ak93 <- factor(
+  group_k00ak93$categoria_k00ak93, 
+  levels = group_k00ak93$categoria_k00ak93[order(group_k00ak93$quantidade, decreasing = FALSE)]
+)
+
+
+#plotar grafico plotly com as cids no eixo y e a quantidade do eixo x
+gbarra_k00ak93 <- plot_ly(
+  data = group_k00ak93,
+  x = ~quantidade,
+  y = ~categoria_k00ak93,
+  type = 'bar',
+  orientation = 'h'  # 'h' para horizontal
+)
+
+# Ajustar layout
+gbarra_k00ak93 <-gbarra_k00ak93 %>%
+  layout(
+    xaxis = list(title = 'Número de Óbitos'),
+    yaxis = list(title = 'Causa Básica do Óbito')
+  )
+
+# Salvar o gráfico HTML
+pasta <- "graficos_comparacao/gbarra_k00ak93.html"
+saveWidget(gbarra_k00ak93, pasta)
+
+
+
+#-------------- Analise mais aprofundada CID F00-F99 -----------------
+
+df_f00af99 <- dados_br_linhaii %>%
+  filter(categoria_causabas == "Transtornos mentais e comportamentais  (F00-F99)")
+
+# Função para categorizar os códigos CID F00-F99 , considerando apenas os três primeiros caracteres
+categorizar_cidf00af99 <- function(causa) {
+  # Extrair os três primeiros caracteres
+  causa_principal <- substr(causa, 1, 3)
+  
+  if (causa_principal >= "F00" & causa_principal <= "F09") {
+    return("Transtornos mentais orgânicos, inclusive os sintomáticos")
+  } else if (causa_principal >= "F10" & causa_principal <= "F19") {
+    return("Transtornos mentais e comportamentais devidos ao uso de substância psicoativa")
+  } else if (causa_principal >= "F20" & causa_principal <= "F29") {
+    return("Esquizofrenia, transtornos esquizotípicos e transtornos delirantes")
+  } else if (causa_principal >= "F30" & causa_principal <= "F39") {
+    return("Transtornos do humor [afetivos]")
+  } else if (causa_principal >= "F40" & causa_principal <= "F48") {
+    return("Transtornos neuróticos, transtornos relacionados com o “stress” e transtornos somatoformes")
+  } else if (causa_principal >= "F50" & causa_principal <= "F59") {
+    return("Síndromes comportamentais associadas a disfunções fisiológicas e a fatores físicos")
+  } else if (causa_principal >= "F60" & causa_principal <= "F69") {
+    return("Transtornos da personalidade e do comportamento do adulto")
+  } else if (causa_principal >= "F70" & causa_principal <= "F79") {
+    return("Retardo mental")
+  } else if (causa_principal >= "F80" & causa_principal <= "F89") {
+    return("Transtornos do desenvolvimento psicológico")
+  } else if (causa_principal >= "F90" & causa_principal <= "F98") {
+    return("Transtornos do comportamento e transtornos emocionais que aparecem habitualmente durante a infância ou a adolescência")
+  } else if (causa_principal >= "F99" & causa_principal <= "F99") {
+    return("Transtorno mental não especificado")
+  } else {
+    return("Categoria desconhecida")
+  }
+}
+
+# Aplicar a função de categorização
+df_f00af99$categoria_f00af99 <- sapply(df_f00af99$CAUSABAS, categorizar_cidf00af99)
+
+
+#Agrupar dados da coluna categoria_causabas
+group_f00af99 <- df_f00af99 %>%
+  group_by(categoria_f00af99) %>%
+  summarize(quantidade = n()) %>%
+  arrange(desc(quantidade))
+
+# Reordenar a variável 
+group_f00af99$categoria_f00af99 <- factor(
+  group_f00af99$categoria_f00af99, 
+  levels = group_f00af99$categoria_f00af99[order(group_f00af99$quantidade, decreasing = FALSE)]
+)
+
+
+#plotar grafico plotly com as cids no eixo y e a quantidade do eixo x
+gbarra_f00af99 <- plot_ly(
+  data = group_f00af99,
+  x = ~quantidade,
+  y = ~categoria_f00af99,
+  type = 'bar',
+  orientation = 'h'  # 'h' para horizontal
+)
+
+# Ajustar layout
+gbarra_f00af99 <-gbarra_f00af99 %>%
+  layout(
+    xaxis = list(title = 'Número de Óbitos'),
+    yaxis = list(title = 'Causa Básica do Óbito')
+  )
+
+# Salvar o gráfico HTML
+pasta <- "graficos_comparacao/gbarra_f00af99.html"
+saveWidget(gbarra_f00af99, pasta)
   
